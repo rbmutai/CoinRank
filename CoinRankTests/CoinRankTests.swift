e53620f1b38f826cd7d7ba9d1aa3b1815b66923c
Robert Mutai <rbmutai@gmail.com>
2025-04-26 12:14:32 +0300

    add get coins unit test

--- CoinRankTests/CoinRankTests.swift
+++ CoinRankTests/CoinRankTests.swift
@@ -10,27 +10,18 @@ import XCTest
 
 final class CoinRankTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testGetCoins() async {
+        let viewModel = CoinRankViewModel(apiService: MockAPIService())
+       
+        await viewModel.getCoinsInfo()
+        
+        XCTAssertTrue(viewModel.coins.count>0)
+        XCTAssertEqual(viewModel.coins[0].uuid, "Qwsogvtv82FCd")
+        XCTAssertEqual(viewModel.coins[0].name, "Bitcoin")
+        XCTAssertEqual(viewModel.coins[0].price, "94800.19141134917")
+        XCTAssertEqual(viewModel.coins[0].volume24h, "30879790744")
+        XCTAssertEqual(viewModel.coins[0].iconUrl, "https://cdn.coinranking.com/bOabBYkcX/bitcoin_btc.svg")
+        XCTAssertEqual(viewModel.coins[0].symbol, "BTC")
     }
 
 }
