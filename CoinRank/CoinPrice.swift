dc06657236db06e9de9f80add551112c32a0d2f6
Robert Mutai <rbmutai@gmail.com>
2025-04-28 17:12:22 +0300

    add coinprice model

---
+++ CoinRank/CoinPrice.swift
@@ -0,0 +1,27 @@
+//
+//  CoinPrice.swift
+//  CoinRank
+//
+//  Created by Robert Mutai on 28/04/2025.
+//
+
+import Foundation
+
+struct CoinPrice: Decodable {
+  
+    let price: String
+    let timeStamp: Int
+  
+    enum CodingKeys: String, CodingKey {
+        case price
+        case timeStamp = "timestamp"
+    }
+ }
+
+ struct CoinPriceHistory: Decodable {
+     let history: [CoinPrice]
+ }
+
+ struct CoinPriceData: Decodable {
+     let data: CoinPriceHistory
+ }
